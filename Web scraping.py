727c1986b66ba8e9e2e067342a63d3274aa4f33a
invalid-code <jessgaming55@gmail.com>
2022-02-18 13:21:14 +0800

    gets html from website and parses it using bs4

--- Web scraping.py
+++ Web scraping.py
@@ -1,1 +1,6 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
+import requests as rq 
+url = "https://kumascans.com/"
+requesting_url = rq.get(url)
+html_doc_soup = BeautifulSoup(requesting_url.text, "html.parser")
+print(html_doc_soup.prettify())

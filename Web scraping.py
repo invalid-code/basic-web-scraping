73274fe031514fbb1d138274ac8a2553c491586f
invalid-code <jessgaming55@gmail.com>
2022-02-25 17:23:26 +0800

    finally got manga and chapter in a dictionary

--- Web scraping.py
+++ Web scraping.py
@@ -1,10 +1,16 @@
-from bs4 import BeautifulSoup 
-import requests as rq 
-read_manga = ["Kenshi o Mezashite", "Tonikaku Kawaii", "Party kara Tsuihou Sareta", "Isekai Meikyuu no Saishinbu", "Hazure Akamadoushi wa Kenja", "Konjiki no Moji Tsukai", "Trinity Seven", "Relentlessly Approaching The Poison-Tongued And Indifferent Beauty To Tickle The Cutesy Reactions Out Of Her", "Shin no Jitsuryoku wa Girigiri", "Youkoso Jitsuryoku Shijou", "Tensei Kenja no Isekai", "The White Mage Who Was Banished From the Hero’s Party Is Picked up by an S Rank Adventurer", "Nido Tensei Shita Shounen", "The World’s Fastest Level up!", "Battle Frenzy", "Ranker Who Lives A Second Time", "The Max Level Hero has Returned!", "Ranker’s Return (Remake)", "Reincarnation of the Battle God", "Leveling With The Gods", "Shijou Saikyou no Mahou", "The Reincarnated Inferior Magic Swordsman", "Otome Game Sekai wa Mob", "Hellmode ~Gamer Who Likes to Speedrun Becomes Peerless in a Parallel World with Obsolete Setting", "Genkai Level 1 kara no Nariagari", "Magia★Record", "The Irregular of the Royal Academy of Magic", "Kami no Techigai de Shindara", "A Returner’s Magic Should Be Special", "Omniscient Reader’s Point of View", "The tutorial tower of the advanced player", "Solo Max-Level Newbie", "Return of the Frozen Player", "Berserk of Gluttony", "Assassin de aru ore no Sutetasu"]
-url = "https://kumascans.com/"
+from bs4 import BeautifulSoup
+import requests as rq
+import re
+latest_manga = {}
+read_manga = ['Kenshi o Mezashite', 'Tonikaku Kawaii', 'Party kara Tsuihou Sareta', 'Isekai Meikyuu no Saishinbu', 'Hazure Akamadoushi wa Kenja', 'Konjiki no Moji Tsukai', 'Trinity Seven', 'Relentlessly Approaching The Poison-Tongued And Indifferent Beauty To Tickle The Cutesy Reactions Out Of Her', 'Shin no Jitsuryoku wa Girigiri', 'Youkoso Jitsuryoku Shijou', 'Tensei Kenja no Isekai', 'The White Mage Who Was Banished From the Hero’s Party Is Picked up by an S Rank Adventurer', 'Nido Tensei Shita Shounen', 'The World’s Fastest Level up!', 'Battle Frenzy', 'Ranker Who Lives A Second Time', 'The Max Level Hero has Returned!',
+              'Ranker’s Return (Remake)', 'Reincarnation of the Battle God', 'Leveling With The Gods', 'Shijou Saikyou no Mahou', 'The Reincarnated Inferior Magic Swordsman', 'Otome Game Sekai wa Mob', 'Hellmode ~Gamer Who Likes to Speedrun Becomes Peerless in a Parallel World with Obsolete Setting', 'Genkai Level 1 kara no Nariagari', 'Magia★Record', 'The Irregular of the Royal Academy of Magic', 'Kami no Techigai de Shindara', 'A Returner’s Magic Should Be Special', 'Omniscient Reader’s Point of View', 'The tutorial tower of the advanced player', 'Solo Max-Level Newbie', 'Return of the Frozen Player', 'Berserk of Gluttony', 'Assassin de aru ore no Sutetasu']
+url = 'https://kumascans.com/'
 html = rq.get(url)
-soup = BeautifulSoup(html.text, "html.parser")
-main_page = soup.find("body")
-latest = main_page.find(class_="releases")
-print(latest)
-# for manga in read_manga:
+soup = BeautifulSoup(html.text, 'html.parser')
+latest_update = soup.find(text='Latest Update').parent.parent.next_siblings
+for manga in latest_update:
+    if manga != '\n':
+        manga_name_chapter = manga.find_all(class_='luf')
+for chapter_name in manga_name_chapter:
+    latest_manga[chapter_name.h4.text] = chapter_name.ul.find_all(text=re.findall('Ch\. \d*\.?\d*', str(chapter_name.ul)))
+print(latest_manga)
